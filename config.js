358b4656b66cdc9dcb5292330c0c0893bb02e81b
prakashash18 <prakashash18@gmail.com>
2021-04-12 14:51:30 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	// authToken is the authorization token to the Particle cloud. You could generate one of these
+	// using the Particle Cloud API, or copy and paste it from the settings page of
+	// https://build.particle.io
+    "authToken": "",
+    
+    // MySQL server configuration. Options are listed here:
+    // https://github.com/mysqljs/mysql#establishing-connections
+    "mysql": {
+    	"host": "localhost",
+    	"user": "root",
+    	"password": "root",
+    	"database": "example_db"
+    }
+};
